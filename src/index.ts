dd46eeffee636f4d81920a9b4bb9faa601358186
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-01-25 16:10:03 -0700

    feat: add hono framework

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+
+const app = new Hono();
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -25,8 +19,8 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
-};
+app.get('/', (c) => {
+	return c.text('Hello Hono!');
+});
+
+export default app;
